aa9810eed7dd748586809b9e6cadd8d397092cbd
KHaeWon <hyen1616@192.168.219.106>
2023-06-07 17:45:57 +0900

    회원 구분 페이지 추가, 회원가입 양식 구분, 대학 이메일 api 넣어보려고 했지만 실패..ㅠㅠ

--- src/main/resources/static/js/member/check_agreement1.js
+++ src/main/resources/static/js/member/check_agreement1.js
@@ -1,3 +1,4 @@
+//고등학생용 회원가입 양식으로 넘어가는 자바스크립트
 function next_signup()  {
 	
   // 전체 체크박스
@@ -8,7 +9,7 @@ function next_signup()  {
     = document.querySelectorAll('input[name="agreement"]:checked');
  
   if(checkboxes.length === checked.length)  {
-      location.href='/signup.do'
+      location.href='/signup1'
 
   }
   
